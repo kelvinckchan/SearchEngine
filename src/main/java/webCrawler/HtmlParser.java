8f720978f68264ea7a2b6b09c236cacc6bd01a57
kelvi_000 <kelvinckchan94@gmail.com>
2017-11-04 16:12:37 +0800

    key sep broken

--- src/main/java/webCrawler/HtmlParser.java
+++ src/main/java/webCrawler/HtmlParser.java
@@ -2,9 +2,11 @@ package webCrawler;
 
 import java.io.IOException;
 import java.lang.Character.UnicodeBlock;
+import java.lang.Character.UnicodeScript;
 import java.net.MalformedURLException;
 import java.net.URL;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.HashSet;
 import java.util.List;
 import java.util.PriorityQueue;
@@ -51,7 +53,7 @@ public class HtmlParser implements Runnable {
 	@Override
 	public void run() {
 		Parser(ParsingURL);
-		
+
 	}
 
 	public void Parser(URL url) {
@@ -59,6 +61,7 @@ public class HtmlParser implements Runnable {
 			Document doc = Jsoup.parse(url, 50000);
 			// System.out.println(doc);
 			ds = new DataStore();
+
 			// Get title
 			Elements WebTitles = doc.select("title");
 			for (Element t : WebTitles) {
@@ -68,62 +71,11 @@ public class HtmlParser implements Runnable {
 
 			// Get words
 			Elements aTagWordsString = doc.select("a");
-			AtomicInteger atomicInteger = new AtomicInteger(0);
+			List<Element> Line = aTagWordsString.stream()
+					.filter(line -> !line.text().replaceAll("[ \\t\\n\\x0B\\f\\r\\d|\\|]", "").equals(""))
+					.collect(Collectors.toList());
 
-			List<Element> Line = aTagWordsString.stream().filter(line -> {
-				String text = line.text().replaceAll("[ \\t\\n\\x0B\\f\\r\\d|\\|]", "");
-				return !text.equals("");
-			}).collect(Collectors.toList());
-			// separate keywords
-			Line.forEach(line -> {
-				// splite by space
-				for (String s : line.text().split("[ \\t\\n\\x0B\\f\\r\\u00a0]")) {
-					// if s is not space only
-					if (!s.trim().replaceAll("[\\t\\n\\x0B\\f\\r\\d|\\|]", "").equals("")) {
-						// If contain Chinese/other character, split dividual character
-						if (containsHanScript(s)) {
-							Character lastChar = null;
-							List<Character> notChinese = new ArrayList<Character>();
-							char[] charArray = s.toCharArray();
-							for (int i = 0; i < charArray.length; i++) {
-								// If contain not Chinese Character, store in notChinese
-								if (!isChineseChar(charArray[i])) {
-									// if Last Character is chinese, create new list
-									if (lastChar != null && isChineseChar(lastChar)) {
-										notChinese = new ArrayList<Character>();
-									}
-									// Store notChinese char
-									notChinese.add(charArray[i]);
-								} else {
-									// Is chinese, directly print out
-//									 System.out.println("*[" + atomicInteger.incrementAndGet() + "]: " +
-//									 charArray[i]);
-									StoreKeywordRow(String.valueOf(charArray[i]), atomicInteger.incrementAndGet());
-								}
-								// if (大@ins{大} || 大@in{s}) print combined notChinese List<Charater> to String
-								// this char = chinese && last char != chinese
-								// or
-								// this char != chinese && is last of the char array
-								if (isChineseChar(charArray[i]) && lastChar != null && !isChineseChar(lastChar)
-										|| !isChineseChar(charArray[i]) && i == charArray.length - 1) {
-									// System.err.println("[" + atomicInteger.incrementAndGet() + "]: " + notChinese
-									// .stream().map(e -> e.toString()).reduce((acc, e) -> acc + e).get());
-									String keyword = notChinese.stream().map(e -> e.toString())
-											.reduce((acc, e) -> acc + e).get();
-									StoreKeywordRow(keyword, atomicInteger.incrementAndGet());
-								}
-								lastChar = charArray[i];
-							}
-						} else {
-							// Not Contain Chinese, print directly
-							// System.err.println(
-							// "[" + atomicInteger.incrementAndGet() + "]: " + s + " C?: " +
-							// containsHanScript(s));
-							StoreKeywordRow(s, atomicInteger.incrementAndGet());
-						}
-					}
-				}
-			});
+			KeywordsSeparator(Line);
 
 			// Get URLs
 			Elements Links = doc.select("a");
@@ -134,13 +86,7 @@ public class HtmlParser implements Runnable {
 				addToUnProcessedURL(link.absUrl("href"));
 				// logger.info("{}\t=>\t{}", link, link.absUrl("href"));
 			});
-			// logger.info("[Parsedurl]: " + url.toString());
 
-			// System.err.println("[" + URLQueue.getProcessedURLSize() + "] ProcessedURL: "
-			// + URLQueue.getProcessedURL());
-			// System.err.println(
-			// "[" + URLQueue.getUnprocessedURLSize() + "] UnprocessedURL: " +
-			// URLQueue.getUnprocessedURL());
 			ds.Store();
 
 		} catch (UnsupportedMimeTypeException ex) {
@@ -157,29 +103,72 @@ public class HtmlParser implements Runnable {
 		ds.addRow(key, 1, WordPos, ParsingURL.toString());
 	}
 
-	HashSet<UnicodeBlock> chineseUnicodeBlocks = new HashSet<UnicodeBlock>() {
-		{
-			add(UnicodeBlock.CJK_COMPATIBILITY);
-			add(UnicodeBlock.CJK_COMPATIBILITY_FORMS);
-			add(UnicodeBlock.CJK_COMPATIBILITY_IDEOGRAPHS);
-			add(UnicodeBlock.CJK_COMPATIBILITY_IDEOGRAPHS_SUPPLEMENT);
-			add(UnicodeBlock.CJK_RADICALS_SUPPLEMENT);
-			add(UnicodeBlock.CJK_SYMBOLS_AND_PUNCTUATION);
-			add(UnicodeBlock.CJK_UNIFIED_IDEOGRAPHS);
-			add(UnicodeBlock.CJK_UNIFIED_IDEOGRAPHS_EXTENSION_A);
-			add(UnicodeBlock.CJK_UNIFIED_IDEOGRAPHS_EXTENSION_B);
-			add(UnicodeBlock.KANGXI_RADICALS);
-			add(UnicodeBlock.IDEOGRAPHIC_DESCRIPTION_CHARACTERS);
-		}
-	};
-
-	public boolean containsHanScript(String s) {
-		return s.codePoints()
-				.anyMatch(codepoint -> Character.UnicodeScript.of(codepoint) == Character.UnicodeScript.HAN);
+	public void KeywordsSeparator(List<Element> Line) {
+		AtomicInteger atomicInteger = new AtomicInteger(0);
+		// separate keywords
+		Line.forEach(line -> {
+			// splite by space
+			for (String s : line.text().replaceAll("[\\pP+0-9&nsbp<>]", "")
+					.split("[ \\t\\n\\x0B\\f\\r\\d\\u00a0|\\|]")) {
+				// if s is not space only
+				if (!s.trim().replaceAll("[ \\t\\n\\x0B\\f\\r\\d|\\|]", "").equals("")) {
+					// If contain Chinese/other character, split dividual character
+					if (containsTargetScript(s)) {
+						Character lastChar = null;
+						List<Character> notChinese = new ArrayList<Character>();
+						char[] charArray = s.toCharArray();
+						for (int i = 0; i < charArray.length; i++) {
+							// If contain not Chinese Character, store in notChinese
+							if (!containsTargetScript(charArray[i])) {
+								// if Last Character is chinese, clear list
+								if (containsTargetScript(lastChar)) {
+									notChinese.clear();
+								}
+								// Store notChinese char
+								notChinese.add(charArray[i]);
+								// if(大@in{s}) this char != chinese && is last of the char array
+								if (i == charArray.length - 1) {
+									// System.err.println("[" + atomicInteger.incrementAndGet() + "]: " + notChinese
+									// .stream().map(e -> e.toString()).reduce((acc, e) -> acc + e).get());
+									String keyword = notChinese.stream().map(c -> c.toString()).reduce((a, b) -> a + b)
+											.get();
+									StoreKeywordRow(keyword, atomicInteger.incrementAndGet());
+								}
+							} else {
+								// if (大@ins{大}) this char == chinese && not Fist Char && last char != chinese
+								if (lastChar != null && !containsTargetScript(lastChar)) {
+									// System.err.println("[" + atomicInteger.incrementAndGet() + "]: " + notChinese
+									// .stream().map(e -> e.toString()).reduce((acc, e) -> acc + e).get());
+									String keyword = notChinese.stream().map(e -> e.toString())
+											.reduce((acc, e) -> acc + e).get();
+									StoreKeywordRow(keyword, atomicInteger.incrementAndGet());
+								} else {
+									// Is chinese, directly print out
+									// System.out.println("[" + atomicInteger.incrementAndGet() + "]: " +
+									// charArray[i]);
+									StoreKeywordRow(String.valueOf(charArray[i]), atomicInteger.incrementAndGet());
+								}
+							}
+							lastChar = charArray[i];
+						}
+					} else {
+						// Not Contain Chinese, print directly
+						// System.err.println("[" + atomicInteger.incrementAndGet() + "]: " + s);
+						StoreKeywordRow(s, atomicInteger.incrementAndGet());
+					}
+				}
+			}
+		});
 	}
 
-	public boolean isChineseChar(Character c) {
-		return chineseUnicodeBlocks.contains(UnicodeBlock.of(c));
+	List<UnicodeScript> TargetScript = Arrays.asList(Character.UnicodeScript.HAN, Character.UnicodeScript.HIRAGANA);
+
+	public boolean containsTargetScript(Character c) {
+		return containsTargetScript(String.valueOf(c));
+	}
+
+	public boolean containsTargetScript(String s) {
+		return s.codePoints().anyMatch(codepoint -> TargetScript.contains(Character.UnicodeScript.of(codepoint)));
 	}
 
 }
