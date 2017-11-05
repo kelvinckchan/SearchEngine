3bb99388c0bbb244e12fc8e641c26f2fb6ba9d53
kelvi_000 <kelvinckchan94@gmail.com>
2017-11-05 14:51:13 +0800

    new

--- src/main/java/testcode/testDS.java
+++ src/main/java/testcode/testDS.java
@@ -4,6 +4,7 @@ import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Comparator;
 import java.util.HashMap;
+import java.util.Iterator;
 import java.util.LinkedHashMap;
 import java.util.List;
 import java.util.Map;
@@ -28,70 +29,105 @@ public class testDS {
 	public void run() {
 		Store();
 		RowMap.forEach((k, v) -> {
-			System.out.printf("Rid(k)-%s: {keyword:\"%s\", Rank:\"%s\", URL:\"%s\"}\n", k, v.Keyword, v.Rank,
-					v.FromURL);
+			System.out.printf("Rid(k)-%s: {keyword:\"%s\", WordPos:%s, Rank:\"%s\", URL:\"%s\"}\n", k, v.Keyword,
+					v.WordNo, v.Rank, v.FromURL);
 		});
 		Search();
 	}
 
 	public void Search() {
 
-		Map<Object, Row> getKeysByValueResult = getKeysByValue(keycol.ColObj, "k1");
-		System.out.println("KBV Result Row: " + getKeysByValueResult);
-		getKeysByValueResult.forEach((id, v) -> {
-			System.out.println("URL KBV: " + v.FromURL);
-		});
+//		Map<Integer, Row> getKeysByValueResult = getKeysByValue(keycol.ColObj, "k1");
+//		System.out.println("KBV Result Row: " + getKeysByValueResult);
+//		getKeysByValueResult.forEach((id, v) -> {
+//			System.out.println("URL KBV: " + v.FromURL);
+//		});
 
-		getSortedRowByColumnValue(keycol.ColObj, "k1").forEach((rank, row) -> {
-			System.out.println("Rank: " + rank + "=> " + " Rid:" + ((Row) row).RowId + " =" + ((Row) row).FromURL);
-		});
 		// sortByValue(rankcol.getColObjMap()).forEach((k, v) -> {
 		// System.out.println("Sorted by Rank: [Rank]: " + k + "=> [RowId]: " + v);
 		// });
 
-		SearchByPhase("k3	k4");
+//		Map<Integer, Row> ResultedRows = getSortedRowByColumnValue(keycol.ColObj, "k1");
+//
+//		ResultedRows.forEach((rank, row) -> {
+//			System.out.println("Rank: " + rank + "=> " + " Rid:" + ((Row) row).RowId + " =" + ((Row) row).FromURL);
+//		});
+//		
+//		
+		 
+
+//		List<String> URLList = ResultedRows.entrySet().stream().map(row -> row.getValue().FromURL)
+//				.collect(Collectors.toList());
+
+//		URLList.forEach(l -> System.out.println(l));
+
+		SearchByPhase("k1 k3	k4");
 
 	}
 
-	public void SearchByPhase(String phase) {
+	public void SearchByPhase(String phrase) {
 
-		System.err.println(Stream.of(phase).map(w -> w.split("\\s+")).flatMap(Arrays::stream)
-				.collect(Collectors.toList()).stream().map(k -> getSortedRowByColumnValue(keycol.ColObj, k))
-				.map(r -> r.values()).collect(Collectors.toSet())
-		// .collect(Collectors.toMap(k->((Map<Integer,
-		// Row>)k).keySet(),k->((Map<Integer, Row>)k).values(), (a,b)->a, HashMap::new
-		// ))
+		
+		for(String s :phrase.split("\\s+")) {
+			   System.out.println("Keyword: "+s +" => "+getSortedRowByColumnValue(keycol.ColObj, s));
+		}
+		
+		
+		Iterator<String> keys = Stream.of(phrase.split("\\s+")).collect(Collectors.toList()).iterator();
 
-		// .filter(m->{
-		//
-		// System.out.println("k: ["+m+"]");
-		// return true;
-		// })
-		// .collect(Collectors.toList())
-		// .forEach(k->{
-		// System.out.println("k: ["+k+"]");
-		// getSortedRowByColumnValue(keycol.ColObj, k);
-		//
-		// })
+		Stream.of(phrase).map(w -> w.split("\\s+")).flatMap(Arrays::stream).filter(k -> {
 
+			getKeysByValue(keycol.ColObj, k).entrySet();
+
+			return true;
+		});
+
+		System.err.println("***" +
+				Stream.of(phrase.split("\\s+")).collect(Collectors.toMap(k -> k,
+						k -> getSortedRowByColumnValue(keycol.ColObj, k), (a, b) -> b, HashMap::new))
 		);
 
-		// for (String s : phase.split("[ \\t\\n\\x0B\\f\\r\\u00a0]")) {
-		// // if s is not space only
-		// if (!s.trim().replaceAll("[\\t\\n\\x0B\\f\\r\\d|\\|]", "").equals("")) {
-		// System.out.println(s);
-		//
-		// }
-		//
-		// }
-
 	}
+
+	// public <T, E> Object[] getRowIDByColumnValue(Map<T, E> map, E value) {
+	// return map.entrySet().stream().filter(entry ->
+	// Objects.equals(entry.getValue(), value)).map(Map.Entry::getKey);
+	// }
 
 	// Yes!!!
 	private <T, E> Map<Integer, Row> getSortedRowByColumnValue(Map<T, E> map, E value) {
 		return map.entrySet().stream().filter(entry -> Objects.equals(entry.getValue(), value)).map(Map.Entry::getKey)
 				.collect(Collectors.toMap(key -> RowMap.get(key).Rank, key -> RowMap.get(key), (a, b) -> a,
 						HashMap::new));
+	}
+
+	// public <T, E> Set<T> getKeysByValue(Map<T, E> map, E value) {
+	public <T, E> Map<Object, Row> getKeysByValue(Map<T, E> map, E value) {
+		Map<Object, Row> result = map.entrySet().stream().filter(entry -> Objects.equals(entry.getValue(), value))
+				.map(Map.Entry::getKey)
+				.collect(Collectors.toMap(key -> key, key -> RowMap.get(key), (a, b) -> a, LinkedHashMap::new));
+
+		System.out.println("getKeysByValue: " + result);
+		return result;
+		// return map.entrySet().stream().filter(entry ->
+		// Objects.equals(entry.getValue(), value)).map(Map.Entry::getKey)
+		// .collect(Collectors.toSet());
+	}
+
+	// notused
+	public Map<Integer, Integer> sortByValue(Map<Integer, Integer> unsortedMap) {
+		// unsortedMap.entrySet().stream().sorted();
+		System.out.println(
+
+				unsortedMap.entrySet().stream().sorted(Map.Entry.comparingByValue())
+						.collect(Collectors.toMap(Map.Entry::getKey, Map.Entry::getValue, (e1, e2) -> e1,
+								LinkedHashMap::new))
+						.entrySet().stream().map(Map.Entry::getKey)
+						.collect(Collectors.toMap(key -> key, key -> RowMap.get(key), (a, b) -> a, LinkedHashMap::new))
+
+		);
+
+		return unsortedMap;
 	}
 
 	public void Store() {
@@ -124,38 +160,6 @@ public class testDS {
 		keycol.addColObj(r.RowId, r.Keyword);
 		rankcol.addColObj(r.RowId, r.Rank);
 		wordnocol.addColObj(r.RowId, r.WordNo);
-	}
-
-	public void SearchKeyword() {
-
-	}
-
-	// public <T, E> Set<T> getKeysByValue(Map<T, E> map, E value) {
-	public <T, E> Map<Object, Row> getKeysByValue(Map<T, E> map, E value) {
-		Map<Object, Row> result = map.entrySet().stream().filter(entry -> Objects.equals(entry.getValue(), value))
-				.map(Map.Entry::getKey)
-				.collect(Collectors.toMap(key -> key, key -> RowMap.get(key), (a, b) -> a, LinkedHashMap::new));
-
-		System.out.println(result);
-		return result;
-		// return map.entrySet().stream().filter(entry ->
-		// Objects.equals(entry.getValue(), value)).map(Map.Entry::getKey)
-		// .collect(Collectors.toSet());
-	}
-
-	public Map<Integer, Integer> sortByValue(Map<Integer, Integer> unsortedMap) {
-		// unsortedMap.entrySet().stream().sorted();
-		System.out.println(
-
-				unsortedMap.entrySet().stream().sorted(Map.Entry.comparingByValue())
-						.collect(Collectors.toMap(Map.Entry::getKey, Map.Entry::getValue, (e1, e2) -> e1,
-								LinkedHashMap::new))
-						.entrySet().stream().map(Map.Entry::getKey)
-						.collect(Collectors.toMap(key -> key, key -> RowMap.get(key), (a, b) -> a, LinkedHashMap::new))
-
-		);
-
-		return unsortedMap;
 	}
 
 }
